dbac60ba67ade66b17388b0c5a742966e9356bf6
alex-chae-kim <alexk2k05@gmail.com>
2026-01-28 18:05:58 -0500

    Add matching engine

--- main.cpp
+++ main.cpp
@@ -1,13 +1,14 @@
 #include <bits/stdc++.h>
+using namespace std;
 
 vector<vector<int>> readInput() {
     int n;
-    std::cin >> n;
+    cin >> n;
     vector<vector<int>> preferences(2 * n, vector<int>(n, -1));
     for (int i = 0; i < 2 * n; i++) {
         for (int j = 0; j < n; j++) {
             int p;
-            std::cin >> p;
+            cin >> p;
             preferences[i][j] = p;
         }
     }
@@ -30,27 +31,69 @@ bool applicantPrefersH1ToH(vector<vector<int>> &preferences, int a, int h, int h
     for (int i = 0; i < n; i++)
     {
         // if we get to h1 first, applicant a prefers h1 to h
-        if (prefer[n + a - 1][i] == h1)
+        if (preferences[n + a][i] == h1 + 1)
             return true;
         // if we get to h first, applicant a does not prefer h1 to h
-        if (prefer[n + a - 1][i] == h)
+        if (preferences[n + a][i] == h + 1)
             return false;
     }
 }
 
 void solve() {
     vector<vector<int>> preferences = readInput();
+    
     bool valid = validateInput(preferences);
     if (!valid) {
-        std::cout << "INVALID INPUT" << std::endl;
+        cout << "INVALID INPUT" << endl;
         return;
     }
     
+    int n = preferences[0].size();
+    vector<int> hospitalMatches(n, -1);
+    vector<int> applicantMatches(n, -1);
+    vector<int> attemptTracker(n, 0);
+    int freeCount = n;
+
+    // while there are unmatched pairs
+    while (freeCount > 0) {
+        // find a free hospital
+        int hospital;
+        for (hospital = 0; hospital < n; hospital++) {
+            if (hospitalMatches[hospital] == -1) {
+                break;
+            }
+        }
+        // find the next applicant that the hospital hasn't already tried to match with
+        int curAttempt = attemptTracker[hospital];
+        attemptTracker[hospital]++;
+        int applicant = preferences[hospital][curAttempt] - 1;
+
+        // check if the applicant is free
+        if (applicantMatches[applicant] == -1) {
+            hospitalMatches[hospital] = applicant;
+            applicantMatches[applicant] = hospital;
+            freeCount--;
+        // otherwise check if that applicant prefers this new hospital to the one they are already paired with
+        } else {
+            int currentAssignment = applicantMatches[applicant];
+            if (applicantPrefersH1ToH(preferences, applicant, currentAssignment, hospital)) {
+                hospitalMatches[hospital] = applicant;
+                applicantMatches[applicant] = hospital;
+                hospitalMatches[currentAssignment] = -1;
+            }
+        }
+    }
+    // print out all results (+1 needed because hospitals and applicants are tracked as 0 indexed in my implementation)
+    for (int i = 0; i < n; i++) {
+        cout << i + 1 << " " << hospitalMatches[i] + 1 << endl;
+    }
+
 }
 
 int main() {
-    std::ios::sync_with_stdio(false);
-    std::cin.tie(0);
+    ios::sync_with_stdio(false);
+    cin.tie(0);
     
     solve();
+    return 0;
 }
